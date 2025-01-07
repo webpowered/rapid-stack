1c9133505d303a66a9f49f5245467fccd067c9a1
Hunar Roop Kahlon <hunar.roop@gmail.com>
2025-01-06 22:35:54 -0800

    feat(route): add root
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

---
+++ app/root.tsx
@@ -0,0 +1,65 @@
+import type { ReactNode } from "react";
+import {
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+	isRouteErrorResponse,
+} from "react-router";
+import type { Route } from "#.react-router/types/app/+types/root.ts";
+
+interface LayoutProps {
+	children: ReactNode;
+}
+
+export function Layout({ children }: LayoutProps) {
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				{children}
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	);
+}
+
+export default function Root() {
+	return <Outlet />;
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+	let message = "Oops!";
+	let details = "An unexpected error occurred.";
+	let stack: string | undefined;
+
+	if (isRouteErrorResponse(error)) {
+		message = error.status === 404 ? "404" : "Error";
+		details =
+			error.status === 404
+				? "The requested page could not be found."
+				: error.statusText || details;
+	} else if (import.meta.env.DEV && error && error instanceof Error) {
+		details = error.message;
+		stack = error.stack;
+	}
+
+	return (
+		<main>
+			<h1>{message}</h1>
+			<p>{details}</p>
+			{stack && (
+				<pre>
+					<code>{stack}</code>
+				</pre>
+			)}
+		</main>
+	);
+}
