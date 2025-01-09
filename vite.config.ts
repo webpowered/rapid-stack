8c33985740f2241bc37487dbad7c65f5def5305a
Hunar Roop Kahlon <hunar.roop@gmail.com>
2025-01-08 21:15:49 -0800

    build(vite): set build target to the same as typescript target
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
 import type { UserConfig } from "vite";
+import { compilerOptions } from "#tsconfig.json";
 
 export default {
+	build: {
+		target: compilerOptions.target,
+	},
 	plugins: [reactRouter()],
 } satisfies UserConfig;
