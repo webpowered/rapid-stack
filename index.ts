e90834ecbe20dded5a48a8aa72925c720c0a772a
Hunar Roop Kahlon <hunar.roop@gmail.com>
2025-01-08 21:26:03 -0800

    feat(server): integrate react-router server
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- index.ts
+++ index.ts
@@ -1,8 +1,33 @@
+import { join } from "node:path";
+import { cwd } from "node:process";
 import { styleText } from "node:util";
-import { serve } from "bun";
+import { file, serve } from "bun";
+import { createRequestHandler } from "react-router";
 
 const server = serve({
-	fetch: () => new Response("Basic Server")
+	fetch: async (request) => {
+		const url = new URL(request.url);
+
+		if (url.pathname.startsWith("/assets")) {
+			const assetPath = join(cwd(), "build", "client", url.pathname);
+			const asset = file(assetPath);
+
+			return new Response(asset);
+		}
+
+		const SERVER_ENTRY_PATH = join(cwd(), "build", "server", "index.js");
+		const serverEntry = file(SERVER_ENTRY_PATH);
+
+		if (await serverEntry.exists()) {
+			const serverEntryModule = await import(SERVER_ENTRY_PATH);
+			const requestHandler = createRequestHandler(serverEntryModule);
+			return requestHandler(request);
+		}
+
+		return new Response("Server entry not found", {
+			status: 500,
+		});
+	}
 });
 
 console.log(styleText("green", `Server is running on ${server.url}`));
